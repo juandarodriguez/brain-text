5954913cbcde194a76caa43baa51ef8cc2906de4
juanda <juanda@juandarodriguez.es>
2019-06-13 18:26:52 +0200

    annotation for BrainText

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,4 @@
-export interface TrainData {
-    label: string,
-    text: string
-}
-
-export interface Result {
-    label: string,
-    text: string,
-    confidence: number,
-    prediction: Object,
-    status: string
-
-}
-
-export function loadTrainDataFromInputDataString(inputDataString: string);
-
-export function addData(traindata: TrainData[]);
-
-export function getTrainData(): TrainData[];
-
-export function train();
-
-export function run(entry: string): Result;
+export function BrainText();
 
 
 
